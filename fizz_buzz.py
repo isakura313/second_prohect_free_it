e8ce7c2ae38886f0b6664d28d0996ef9c2e122e0
Paul <isakura313@gmail.com>
2020-08-30 12:43:12 +0300

    asdsad

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -5,13 +5,27 @@
  Числа, делящиеся на три и пять одновременно заменяются на fizz buzz.
  В нашей задаче это будет первые 100 чисел
 """
+data = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz Buzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'Fizz Buzz']
 
-for i in range(100):
+data_trial = []
+
+for i in range(1, 31):
     if i % 3 == 0 and i % 5 == 0:
-        print("FIZZ BUZZ")
+        print("Fizz Buzz")
+        data_trial.append("Fizz Buzz")
     elif i % 3 == 0:
-        print("FIZZ")
+        print("Fizz")
+        data_trial.append("Fizz")
     elif i % 5 == 0:
-        print("BUZZ")
+        print("Buzz")
+        data_trial.append("Buzz")
     else:
         print(i)
+        data_trial.append(i)
+
+print(data_trial)
+print(data)
+if data_trial == data:
+    print("Все правильно")
+else:
+    print("как-то так")
