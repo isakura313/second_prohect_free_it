166f02cfdf65e695954688ed5038b51f38e943ca
Paul <isakura313@gmail.com>
2020-08-30 11:25:01 +0300

    add ficha

--- ugadaika.py
+++ ugadaika.py
@@ -2,13 +2,20 @@ import random
 rand = random.randint(1, 30)
 count = 0
 while True:
-    predict = input("Попробуйте угадать число от 1 до 30:\n")
-    if int(predict) == rand:
+    print("У вас только 6 попыток")
+    predict = int(input("Попробуйте угадать число от 1 до 30:\n"))
+    if predict == rand:
         print("Отлично, вы угадали!")
         break
-    elif count == 9:
+    elif count == 6:
         print("game over", "Настоящие число было", rand)
         break
+    elif predict > rand:
+        print("меньше возьми")
+        count += 1
+    elif predict < rand:
+        print("больше возьми")
+        count += 1
     else:
         print("Видимо нет... попробуйте еще раз")
         count += 1
