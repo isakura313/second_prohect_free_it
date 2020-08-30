7f3e733042c95da93684718bd57c6450c31a4f1e
Paul <isakura313@gmail.com>
2020-08-30 12:20:56 +0300

    adsdsad

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -7,3 +7,11 @@
 """
 
 for i in range(100):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FIZZ BUZZ")
+    elif i % 3 == 0:
+        print("FIZZ")
+    elif i % 5 == 0:
+        print("BUZZ")
+    else:
+        print(i)
